f7e5b2c59c6ebf4f27d519ff941daab8f85d1408
Rupak Saha <rupak10987@gmail.com>
2023-05-22 09:24:57 +0600

    final

--- Fuzzy_car_controller_implementation/Fuzzy_car.cpp
+++ Fuzzy_car_controller_implementation/Fuzzy_car.cpp
@@ -2,8 +2,6 @@
 #include<time.h>
 #include<conio.h>
 #include<math.h>
-#include<graphics.h>
-#include"Graphic_visualization.h"
 int NL=30,NM=60,NS=90,ZE=120,PS=150,PM=180,PL=210;
 struct uXX
 {
@@ -27,19 +25,8 @@ public:
     float accelaration=0;
     float speed=210;
     float throttle=0;
-    float mass=210;
-    void update_car()
-    {
-        this->accelaration=this->throttle/mass; //a=f/m
-        this->speed=this->speed+this->accelaration*delta_time; //v=u+at
-        this->posY-=this->speed*delta_time;//slightly changed
-        if(posY<=-25)
-            posY=525;
-    }
-    void set_throttle(float Fthrottle)
-    {
-        this->throttle=Fthrottle;
-    }
+    float mass=1;
+
 };
 
 //func declarations
@@ -53,19 +40,16 @@ void membership(class CAR* car_FUZZY,const char* str,struct uXX* miu);
 
 int main()
 {
-    fuzzy_graphic::init_window(500,500);
     //initialize the two cars one is random speed
     //another is controlled by fuzzy controller
     class CAR *car_AUTO=new CAR();
 
     class CAR *car_FUZZY=new CAR();
-    car_FUZZY->accelaration=0;
-    car_FUZZY->speed=197;
-    car_FUZZY->throttle=0;
-    car_FUZZY->posX=75;
+    std::cout<<"Speed Difference = ";
+    std::cin>>car_FUZZY->speed;
+    std::cout<<"acceleration = ";
+    std::cin>>car_FUZZY->accelaration;
 
-    while(true)
-    {
 //DELTATIME AND FPS COUNTING
         delta_time =clock() - oldTime;//0.001;//
         delta_time/=1000;
@@ -79,13 +63,8 @@ int main()
         membership(car_FUZZY,"accelaration",&miu_accelaration);
 
         float THROTTLE=Apply_Rule_Base_And_Defuzzy(&miu_speed,&miu_accelaration);
-        std::cout<<THROTTLE<<std::endl;
-        car_FUZZY->set_throttle(THROTTLE);
-        car_FUZZY->update_car();
-        car_AUTO->update_car();
-        //graphics goes here;
-        fuzzy_graphic::visualize(car_FUZZY->posX,car_FUZZY->posY,car_AUTO->posX,car_AUTO->posY);
-    }
+        std::cout<<"THROTTLE = sum(Ai*Xi) / sum(Ai)"<<std::endl;
+        std::cout<<"THROTTLE = "<<THROTTLE<<std::endl;
 
     return 0;
 }
@@ -113,7 +92,6 @@ float CALC_AREA(float height)
     float b;
     b=a*(1-height);
     float area=0.5*height*(a+b);
-    std::cout<<"area="<<area<<std::endl;
     return area;
 }
 
@@ -121,30 +99,38 @@ float CALC_AREA(float height)
 float Apply_Rule_Base_And_Defuzzy(struct uXX* speed,struct uXX* accelaration)
 {
 ///RULE BASE
-    std::cout<<"RULE\n";
+    std::cout<<"\n\nRULE\n";
 //if(speed Difference is NL) (and Acceleration is ZE) then throttle is PL
     float hPL=minimum(speed->uNL,accelaration->uZE);
+    std::cout<<"min(uNL,uZE)=hPL\n";
     std::cout<<"min("<<speed->uNL<<","<<accelaration->uZE<<") = "<<hPL<<"\n";
 //if(speed Difference is ZE) (and Acceleration is NL) then throttle is PL
     float hPL2=minimum(speed->uZE,accelaration->uNL);
+    std::cout<<"min(uZE,uNL)=hPL2\n";
     std::cout<<"min("<<speed->uZE<<","<<accelaration->uNL<<") = "<<hPL2<<"\n";
 //if(speed Difference is NM) (and Acceleration is ZE) then throttle is PM
     float hPM=minimum(speed->uNM,accelaration->uZE);
+    std::cout<<"min(uNM,uZE)=hPM\n";
     std::cout<<"min("<<speed->uNM<<","<<accelaration->uZE<<") = "<<hPM<<"\n";
 //if(speed Difference is NS) (and Acceleration is PS) then throttle is PS
     float hPS=minimum(speed->uNS,accelaration->uPS);
+    std::cout<<"min(uNS,uPS)=hPS\n";
     std::cout<<"min("<<speed->uNS<<","<<accelaration->uPS<<") = "<<hPS<<"\n";
 //if(speed Difference is PS) (and Acceleration is NS) then throttle is NS
     float hNS=minimum(speed->uPS,accelaration->uNS);
+    std::cout<<"min(uPS,uNS)=hNS\n";
     std::cout<<"min("<<speed->uPS<<","<<accelaration->uNS<<") = "<<hNS<<"\n";
 //if(speed Difference is PL) (and Acceleration is ZE) then throttle is NL
     float hNL=minimum(speed->uPL,accelaration->uZE);
+    std::cout<<"min(uPL,uZE)=hNL\n";
     std::cout<<"min("<<speed->uPL<<","<<accelaration->uZE<<") = "<<hNL<<"\n";
 //if(speed Difference is ZE) (and Acceleration is NS) then throttle is PS
     float hPS2=minimum(speed->uZE,accelaration->uNS);
+    std::cout<<"min(uZE,uNS)=hPS2\n";
     std::cout<<"min("<<speed->uZE<<","<<accelaration->uNS<<") = "<<hPS2<<"\n";
 //if(speed Difference is ZE) (and Acceleration is NM) then throttle is PM
     float hPM2=minimum(speed->uZE,accelaration->uNM);
+    std::cout<<"min(uZE,uNM)=hPM2\n";
     std::cout<<"min("<<speed->uZE<<","<<accelaration->uNM<<") = "<<hPM2<<"\n";
 
 //taking the maximum of the rule that produc e same throttle
@@ -153,9 +139,11 @@ float Apply_Rule_Base_And_Defuzzy(struct uXX* speed,struct uXX* accelaration)
     hPS=maximum(hPS2,hPS);
 
 ///DEFUZZYFICATION
-//sum(Ai*Xi)/sum(Ai)
+
 float SUM_Ai_Xi=(CALC_AREA(hPL)*PL+CALC_AREA(hPM)*PM+CALC_AREA(hPS)*PS+CALC_AREA(hNS)*NS+CALC_AREA(hNL)*NL);
+std::cout<<"sum(Ai*Xi) = "<<SUM_Ai_Xi<<"\n";
 float SUM_AI=(CALC_AREA(hPL)+CALC_AREA(hPM)+CALC_AREA(hPS)+CALC_AREA(hNS)+CALC_AREA(hNL));
+std::cout<<"sum(Ai) = "<<SUM_AI<<"\n";
 if(SUM_AI!=0)
 return SUM_Ai_Xi/SUM_AI;
 else
@@ -187,6 +175,15 @@ void membership(class CAR* car_FUZZY,const char* str,struct uXX* miu)
         x=car_FUZZY->accelaration;
 
     int inp=(int)(x/30);
+    if(inp<0)
+    {
+        miu->uNL=1.0;
+    }
+    else if(inp>=8)
+    {
+        miu->uPL=1.0;
+    }
+    else
     switch(inp)
     {
     case 0://less than 30
@@ -196,7 +193,7 @@ void membership(class CAR* car_FUZZY,const char* str,struct uXX* miu)
     }
     case 1://31-60
     {
-        miu->uNL=calculate_miu(x-(NL-30),NL+30-x,0.033,0.033);
+        miu->uNL=calculate_miu(x-(NL-30),NL+30-x,0,0.033);
         miu->uNM=calculate_miu(x-(NM-30),NM+30-x,0.033,0.033);
         break;
     }
@@ -232,7 +229,7 @@ void membership(class CAR* car_FUZZY,const char* str,struct uXX* miu)
     }
     case 7:
     {
-        miu->uPL=calculate_miu(x-(PL-30),PL+30-x,0.033,0.033);
+        miu->uPL=calculate_miu(x-(PL-30),PL+30-x,0.033,0);
         break;
     }
     }
--- Fuzzy_car_controller_implementation/Graphic_visualization.h
+++ Fuzzy_car_controller_implementation/Graphic_visualization.h
@@ -3,13 +3,22 @@ namespace fuzzy_graphic
 {
 //the auto car should always be placed at middle
 //and the relative positon of the fuzzy_car should be changed
-void visualize(float x1, float y1,float x2, float y2)
+void visualize(float x1, float y1,float x2, float y2,float sp1,float a1,float t1,float sp2)
 {
 cleardevice();
 int width=24,height=50;
-rectangle(x1-width/2,y1-height/2,x1+width/2,y1+height/2);
 
+//first car
+rectangle(x1-width/2,y1-height/2,x1+width/2,y1+height/2);
+char chr[100];
+sprintf(chr,"speed=%f,\nacceleration=%f\nthrust=%f",sp1,a1,t1);
+outtextxy(x1,y1,chr);
+
+//2nd car
 rectangle(x2-width/2,y2-height/2,x2+width/2,y2+height/2);
+char chr1[100];
+sprintf(chr1,"speed=%f,\nacceleration=%f\nthrust=%f",sp2,0,0);
+outtextxy(x2,y2,chr1);
 
 swapbuffers();
 }
